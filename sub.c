22c109443035e81217b7995be53939de0e132ef5
iiCloudex <marcoabbpm@hotmail.com>
2018-09-10 21:58:53 -0700

    Added comments

--- sub.c
+++ sub.c
@@ -11,22 +11,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+
+// Description: 
+// This function opens the help file and
+// prints it to the screen.
 void helpPrompt()
 {
 	FILE *help;
 	char c;
-
-	help = freopen("help.txt", "r", stdin);
 	
-	while( (c = getc(help)) != EOF)
-	{
-		fputc(c, stdout);
+	//Open help.txt
+	help = freopen("help.txt", "r", stdin); 
+	
+	if(help)
+	{	
+		while( (c = getc(help)) != EOF)
+		{	
+			fputc(c, stdout);
+		}
+	
+		fclose(help);
+		exit(0);
 	}
-	
-	fclose(help);
-	
+
 }
 
+
+// Description:
+// This function takes an input file and replaces
+// every character that is also in toRepl with the
+// corresponding character in isRepl then prints it
+// to the output file.
 void replace(char *toRepl, char *isRepl, FILE *input, FILE *output)
 {
 	FILE *ir;
@@ -35,6 +52,7 @@ void replace(char *toRepl, char *isRepl, FILE *input, FILE *output)
 	int minSearch = strlen(toRepl);
 	char *i;
 
+	//Find the minimum value to search for
 	if(strlen(isRepl) < minSearch)
 	{
 		minSearch = strlen(isRepl);
@@ -43,17 +61,21 @@ void replace(char *toRepl, char *isRepl, FILE *input, FILE *output)
 	ir = freopen(input, "r", stdin);
 	or = freopen(output, "w", stdout);
 
-	while( (i = getc(ir)) != EOF)
+	while( (i = getc(ir)) != EOF) //go char by char
 	{	
-		index = -1;
+		index = -1; //-1 for not found
+
+		//search if i is in toRepl
 		for(int j = 0; j < strlen(toRepl) && j < minSearch; j++)
 		{
+			//if it is then grab the index
 			if( toRepl[j] == i)
 			{
 				index = j;
 			}
 		}
 		
+		//if found replace it
 		if(index >= 0 && isRepl[index] != EOF)
 		{
 			fputc(isRepl[index], or);
@@ -81,31 +103,27 @@ int main(int argc, char **argv)
 
 	for(int i = 1; i < argc; i++)
 	{
-		userInput = argv[i];
+		userInput = argv[i]; //grab first string
 		
 		switch(userInput[1])
 		{
 			case '-':
 				//Set variable
-				toRepl = userInput + 2;
-				printf("replacing: %s\n", toRepl);
+				toRepl = userInput + 2;	
 				break;
 			case '+':
 				//Set variable
-				isRepl = userInput + 2;
-				printf("with: %s\n", isRepl);
+				isRepl = userInput +2;
 				break;
 			case 'i':
 				//Set input file
 				i++;
 				inputFile = argv[i];
-				printf("input file: %s\n", inputFile);
 				break;
 			case 'o':
 				//Set output file
 				i++;
 				outputFile = argv[i];
-				printf("output file: %s\n", outputFile);
 				break;
 			case 'h':
 				//print help	
@@ -115,6 +133,7 @@ int main(int argc, char **argv)
 				//Error
 				printf("misuse of the sub utility\n");
 				printf("type sub -h for help\n");
+				exit(1);
 				break;
 		}
 
